943c785b9c39872c822ef8450ee418fde77ffc26
Yadvir Kaur <yadvirkaur15@gmail.com>
2024-01-25 13:04:52 +0530

    Build a Navbar with CharkaUI: HStack, Image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 function App() {
   return (
     <Grid
       templateAreas={{ base: `" nav" "main"`, lg: `"nav nav" "aside main"` }}
     >
-      <GridItem area="nav" bg="orange.300">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="pink.300">
