b0ef747568f01f334e10a93c04fc3405b9f7021e
Yadvir Kaur <yadvirkaur15@gmail.com>
2024-01-26 13:34:59 +0530

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { Badge } from '@chakra-ui/react';
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
+  return (
+    <div>
+      <Badge
+        fontSize="14px"
+        paddingX={2}
+        colorScheme={color}
+        borderRadius="4px"
+      >
+        {score}
+      </Badge>
+    </div>
+  );
+};
+
+export default CriticScore;
