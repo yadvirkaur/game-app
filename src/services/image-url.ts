f2a9c81c316c5a07b6dd21804e782b1bb77c97d5
Yadvir Kaur <yadvirkaur15@gmail.com>
2024-01-30 01:41:21 +0530

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,8 @@
-
-
-const getCroppedImageUrl= (url: string)=>{
-
-const index =url.indexOf('media/') + 'media/'.length;
-return url.slice(0,index)+ 'crop/600/400/' + url.slice(index);
-}
+const getCroppedImageUrl = (url: string) => {
+    if(!url) return '';
+    
+  const index = url.indexOf('media/') + 'media/'.length;
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+};
 
 export default getCroppedImageUrl;
