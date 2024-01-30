f816495ab109d825e3c84f8d6a20f20c133f7334
Yadvir Kaur <yadvirkaur15@gmail.com>
2024-01-30 13:26:35 +0530

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+// import useData from "./useData";
+import genres from '../data/genres'
 
 export interface Genre {
   id: number;
@@ -7,5 +8,8 @@ export interface Genre {
 }
 
 
-const useGenres = () => useData<Genre>("/genres")
+// const useGenres = () => useData<Genre>("/genres")
+//we shipped genre list using static data as it will hardly change and this will save the extra request to server and improves performance
+const useGenres = () => ({ data: genres, error:false, isLoading:null })
+
 export default useGenres;
